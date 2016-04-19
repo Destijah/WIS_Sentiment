36f7e2152f28aa556ff95a794d32b680df9fcfcd
Daniel McIntyre <dannymac92@live.com>
2016-04-18 21:06:06 -0400

    Sanity Checking
    - Sanity/error checking in place for the front end.
    - Updated the search api notes and limitations.
    - Added tweet creation date to displayed tweets.

--- wis_sentiment/routes/index.js
+++ wis_sentiment/routes/index.js
@@ -18,23 +18,37 @@ router.post('/tweets', function(req, res) {
 	var tweet = req.body.tweet;
 	var specifier = req.body.specifier;
 	
-	// TODO: sanity checking for start vs end dates
-	// TODO: sanity checking for blank input
-	// TODO: force a start and end date for topic
-	// TODO: check tweet id as integer
-	
-	if (specifier == "topic")
+	if (tweet == "")
 	{
-		controller.topic(tweet, start, end, 100, function(body){
-			// Filter out data that is undefined
-			tweets = body['tweets'].filter(function(x) { return x != undefined });
-			error = body['error']
-			sentiment = body['sentiment']
-			res.render('tweets', { tweets: tweets, sentiment: sentiment, error: error });
-		});
+		res.render('tweets', { error: "The tweet input field cannot be blank!" });
+	}
+	else if (specifier == "topic")
+	{
+		if (start == "" || end == "")
+		{
+			res.render('tweets', { error: "Both a start and end date must be provided to search tweets by topic!" });
+		}
+		else if (end < start)
+		{
+			res.render('tweets', { error: "Start date cannot be more recent than the end date!" });
+		}
+		else
+		{
+			controller.topic(tweet, start, end, 100, function(body){
+				// Filter out data that is undefined
+				tweets = body['tweets'].filter(function(x) { return x != undefined });
+				error = body['error']
+				sentiment = body['sentiment']
+				res.render('tweets', { tweets: tweets, sentiment: sentiment, error: error });
+			});
+		}
 	}
 	else if (specifier == "tweet")
 	{
+		if (isNaN(tweet) || tweet.indexOf('.') != -1)
+		{
+			res.render('tweets', { error: "Tweet id must be an integer!" });
+		}
 		controller.id(tweet, function(body){
 			tweets = body['tweets']
 			error = body['error']
